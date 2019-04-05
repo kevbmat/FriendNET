9570bdaeaf1c806fb94e31c8b1187849418c6bae
Kevin Mattappally <kevinmat@outlook.com>
2019-04-05 16:19:37 -0700

    working out how to store the friends

--- FriendNet.java
+++ FriendNet.java
@@ -1,4 +1,7 @@
+
 public class FriendNet {
+
+    private Map<String, String> friendMap = new HashMap<>();
     public static void main(String[] args) {
         File file = new File("Friends.txt");
         Scanner inputFile = new Scanner(file);
