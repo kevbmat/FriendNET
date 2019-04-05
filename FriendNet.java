6be59028d9f5385923c3a32ee8ac5d25a0a020a3
Kevin Mattappally <kevinmat@outlook.com>
2019-04-05 16:39:20 -0700

    menu formatted

--- FriendNet.java
+++ FriendNet.java
@@ -5,5 +5,32 @@ public class FriendNet {
     public static void main(String[] args) {
         File file = new File("Friends.txt");
         Scanner inputFile = new Scanner(file);
+
+    }
+
+    public static void showMenu() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What do you want to do?");
+        System.out.println("1) Check if user exists");
+        System.out.println("2) Check connection between users");
+        System.out.println("3) Quit");
+        char choice = sc.next().charAt(0);
+        switch (choice) {
+            case '1':
+                // Check if user exists
+                break;
+            case '2':
+                // Check connection between users
+                break;
+            case '3':
+                // Quit
+                System.out.println("Exiting...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid Choice");
+                showMenu();
+        }
+
     }
 }
