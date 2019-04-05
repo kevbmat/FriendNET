5b932d4b049cf1d441cd026f8585522ae2b4b648
Kevin Mattappally <kevinmat@outlook.com>
2019-04-05 16:07:30 -0700

    files set up

---
+++ FriendNet.java
@@ -0,0 +1,6 @@
+public class FriendNet {
+    public static void main(String[] args) {
+        File file = new File("Friends.txt");
+        Scanner inputFile = new Scanner(file);
+    }
+}
---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	javac KnapsackDynamic.java;
+	java KnapsackDynamic;
